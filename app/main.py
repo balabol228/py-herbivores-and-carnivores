f52b1c1f27c2cab515d9aeef9656bd7d3bff5da4
Denys Baliuk <denuk14228@gmail.com>
2026-02-24 01:45:47 +0300

    first try done project

--- app/main.py
+++ app/main.py
@@ -1,1 +1,28 @@
-# write your code here
+class Animal:
+    alive = []
+    def __init__(self: name,):
+        self.health = 100
+        self.hidden = False
+        Animal.alive.append(self)
+
+    def __repr__(self):
+        return f"{{Name: {self.name}, Health: {self.health}, Hidden: {self.hidden}}}"
+
+    def die(self):
+        if self.health <= 0:
+            Animal.alive.remove(self)
+
+    def Herbivore(self: hide):
+        self.hidden = not self.hidden
+
+class Carnivore(Animal):
+    def bite(self, target):
+        isinstance(target, Herbivore)
+        if isinstance(target, Herbivore) and target.hidden == False:
+            target.health -= 50
+
+            if target.healt <= 0:
+                Animal.alive.remove(target)
+
+                if target in Animal.alive:
+                    Animal.alive.remove(target)
